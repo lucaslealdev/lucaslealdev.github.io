d28f6c06a4363ea51cde99b31effc53a71b38685
Lucas Leal <lealzitooo@gmail.com>
2022-01-31 16:54:43 -0300

    Adiciona relógio animado

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Olá galerinha! Aqui eu publico tech-demos e conceitos.
 
 [Balões de Order](https://lucaslealdev.github.io/baloon-order/) - Ferramenta visual para construção do conhecimento da propriedade `order` do `flexbox`
 
+[Relógio animado](https://lucaslealdev.github.io/animated-clock/) - Relógio utilizando pseudo-elementos, flex, loops, e timeout.
---
+++ animated-clock/script.js
@@ -0,0 +1,21 @@
+let elems = [
+    document.getElementById('hour1'),
+    document.getElementById('hour2'),
+    document.getElementById('minute1'),
+    document.getElementById('minute2'),
+    document.getElementById('second1'),
+    document.getElementById('second2'),
+];
+
+function update(){
+    const d = new Date();
+    const hour = ('0' + d.getHours()).slice(-2);
+    const minute = ('0' + d.getMinutes()).slice(-2);
+    const second = ('0' + d.getSeconds()).slice(-2);
+    const now = hour + minute + second;
+    for (let index = 0; index < elems.length; index++) {
+        elems[index].className = `timebar n${now[index]}`;
+    }
+}
+update();
+setInterval(update,1000);
